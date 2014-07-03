81f93ae786cff3adfb74e400370e7f1f80ee422a
oopmonkey <dddworld@gmail.com>
2014-07-02 22:09:15 -0400

    ex 3 now uses floating point numbers

--- ex3.rb
+++ ex3.rb
@@ -1,22 +1,22 @@
 puts "I will now count my chickens:"
-puts "Hens", 25 + 30 / 6
-puts "Roosters", 100 - 25 * 3 % 4
+puts "Hens", 25.0 + 30.0 / 6.0
+puts "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 puts "Now I will count the eggs:"
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
-puts "Is it true that 3 + 2 < 5 - 7?"
-puts 3 + 2 < 5 - 7
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+puts 3.0 + 2.0 < 5.0 - 7.0
 
-puts "What is 3 + 2?", 3 + 2
-puts "What is 5 - 7?", 5 - 7
+puts "What is 3.0 + 2.0?", 3.0 + 2.0
+puts "What is 5.0 - 7.0?", 5.0 - 7.0
 
 puts "Oh, that's why it's false."
 
 puts "How about some more."
 
-puts "Is it greater?", 5 > -2
-puts "Is it greater or equal?", 5 >= -2
-puts "Is it less than equal?", 5 <= -2
+puts "Is it greater?", 5.0 > -2.0
+puts "Is it greater or equal?", 5.0 >= -2.0
+puts "Is it less than equal?", 5.0 <= -2.0
 
-puts "What is 1000000 * 99999999 / 2?", 1000000 * 99999999 / 2
+puts "What is 1,000,000.0 * 99,999,999.0 / 2.0?", 1000000.0 * 99999999.0 / 2.0
