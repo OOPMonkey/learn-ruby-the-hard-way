0318a12e75bcb3f81590273fd54515ae198937a1
oopmonkey <dddworld@gmail.com>
2014-07-04 10:19:03 -0400

    ex11 base and extra credit done

---
+++ ex11_extra_credit.rb
@@ -0,0 +1,7 @@
+print "What is your Home World? "
+homeWorld = gets.chomp()
+
+print "What is your career? "
+career = gets.chomp()
+
+puts "Your home world is #{homeWorld} and your career is #{career}."
